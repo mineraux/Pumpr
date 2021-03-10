a1ecbd7fdf9867ff6bb88294d26229f7d5fbe934
Robin <robin.minervini@gmail.com>
2021-03-10 12:30:55 +0100

    Clean Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,36 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <button @click="onClick">Get data</button>
-    <ul>
-      <li v-for="name in names" :key="name">{{name}}</li>
-    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue";
 import { db } from "@/firebase/db";
-import { mapGetters } from "vuex";
 
-@Component({
-  components: {
-    HelloWorld
-  },
-  computed: mapGetters(['names'])
-})
+@Component({})
 export default class Home extends Vue {
   onClick(): void {
     db.collection('myCollection')
     .get()
     .then(querySnapshot => {
       const documents = querySnapshot.docs.map(doc => doc.data())
-      // do something with documents
 
       console.log(documents)
     })
   }
-
-  names!:any[]
 }
 </script>
