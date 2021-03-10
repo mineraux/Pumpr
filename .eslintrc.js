0b3ee7420de6432756ea0a6012db21146a8b82a2
Robin <robin.minervini@gmail.com>
2021-03-10 12:26:50 +0100

    Updater prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,15 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "warn",
+      {
+        "singleQuote": true,
+        "semi": false,
+        "trailingComma": "none"
+      }
+    ]
   },
   overrides: [
     {
